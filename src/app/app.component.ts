4debc4ae856a90297b210e8cb7d95db75149d092
Setayesh <75073451+setykarimi@users.noreply.github.com>
2025-05-23 14:53:20 +0330

    🌿feat: Add master component in app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
+import { MasterComponent } from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RolesComponent],
+  imports: [RouterOutlet,RolesComponent, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
