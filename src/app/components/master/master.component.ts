ab562c0d3d0479bdffe94dbe847f26f7abbd7389
Setayesh <75073451+setykarimi@users.noreply.github.com>
2025-06-04 12:16:06 +0330

    🌿feat: Create function for changing the tab

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DesignationComponent } from '../designation/designation.component';
+import { RolesComponent } from '../roles/roles.component';
 
 @Component({
   selector: 'app-master',
-  imports: [],
+  imports: [CommonModule, RolesComponent, DesignationComponent],
   templateUrl: './master.component.html',
-  styleUrl: './master.component.css'
+  styleUrl: './master.component.css',
 })
 export class MasterComponent {
-
+  currentComponent = 'Roles';
+  changeTab(tab: string) {
+    this.currentComponent = tab;
+  }
 }
