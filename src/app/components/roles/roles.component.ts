18c317292a5e51576eeceffa2dd9f3db23a56663
Setayesh <75073451+setykarimi@users.noreply.github.com>
2025-06-04 12:16:26 +0330

    🌿feat: Fetch roles data from api

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -7,15 +9,19 @@ import {FormsModule} from '@angular/forms'
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css',
 })
+export class RolesComponent implements OnInit {
+  http = inject(HttpClient);
+  roleList: IRole[] = [];
 
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
 
-export class RolesComponent {
-  firstName: string = 'Angular Tutorial';
-  angularVersion = 'Version 19';
-
-  version: number = 18;
-
-  isActive: boolean = false;
-
-  currentDate: Date = new Date();
+  getAllRoles() {
+    this.http
+      .get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles')
+      .subscribe((res: any) => {
+        this.roleList = res.data;
+      });
+  }
 }
