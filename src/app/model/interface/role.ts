f9fdfec5d0856fe70a32318729b5a8409f68b443
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-07 15:47:21 +0330

    feat: Add new interface

--- src/app/model/interface/role.ts
+++ src/app/model/interface/role.ts
@@ -2,6 +2,10 @@ export interface IRole {
   roleId: number;
   role: string;
 }
+export interface IDesignation {
+  designationId: number;
+  designation: string;
+}
 
 export interface APIResponseModel {
   message: string;
