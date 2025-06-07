e24c41d408ce329c4ca5e9b545e1ebc9f0ad4a97
Setayesh <75073451+setykarimi@users.noreply.github.com>
2025-06-07 06:47:10 +0330

    🌿feat: Call api from master service

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
   imports: [],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit{
+  masterService = inject(MasterService);
+  ngOnInit(): void {
+this.masterService.getDesignations().subscribe((result:APIResponseModel)=>{
+  
+})    
+  }
+
 
 }
