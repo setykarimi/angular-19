499b9f5c76710c0cd5eb870aebaf072ea094899e
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-07 15:47:38 +0330

    feat: Add commonModule

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,20 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
 import { MasterService } from '../../services/master.service';
-import { APIResponseModel } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-designation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css',
 })
-export class DesignationComponent implements OnInit{
+export class DesignationComponent implements OnInit {
+  designationList: IDesignation[] = [];
   masterService = inject(MasterService);
+
   ngOnInit(): void {
-this.masterService.getDesignations().subscribe((result:APIResponseModel)=>{
-  
-})    
+    this.masterService.getDesignations().subscribe(
+      (result: APIResponseModel) => {
+        this.designationList = result.data;
+      },
+      (error) => {
+        alert('API error / Network Down');
+      }
+    );
   }
-
-
 }
