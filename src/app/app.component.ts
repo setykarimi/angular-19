7f23ce9809348f820c6edc9e120587b77937b6d1
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-07 16:20:48 +0330

    feat: Create new routes and add navbar in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from './components/roles/roles.component';
-import { MasterComponent } from './components/master/master.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RolesComponent, MasterComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular_19_tutorial';
