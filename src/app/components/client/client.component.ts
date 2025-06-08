7f53726e1c124a609496e76c2090c9b7ed290910
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-08 15:15:52 +0330

    🌿feat: Add load client and delete and update api in clientComponent page

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,16 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Client } from '../../model/class/Client';
+import { APIResponseModel } from '../../model/interface/role';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css',
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
   clientObj: Client = new Client();
   clientList: Client[] = [];
 
-  onSaveClient() {}
+  clientService = inject(ClientService);
+
+  ngOnInit(): void {
+    this.loadClient();
+  }
+
+  loadClient() {
+    this.clientService.getAllClients().subscribe((res: APIResponseModel) => {
+      this.clientList = res.data;
+    });
+  }
+
+  onSaveClient() {
+    this.clientService
+      .addUpdate(this.clientObj)
+      .subscribe((res: APIResponseModel) => {
+        if (res.result) {
+          alert('Client Created Success');
+          this.loadClient();
+          this.clientObj = new Client();
+        } else {
+          alert(res.message);
+        }
+      });
+  }
+
+  onDelete(id: number) {
+    const isDelete = confirm('Are you sure want to delete?');
+    if (isDelete == true) {
+      this.clientService
+        .deleteClientByID(id)
+        .subscribe((res: APIResponseModel) => {
+          if (res.result) {
+            alert('Client deleted successfully');
+            this.loadClient();
+            this.clientObj = new Client();
+          } else {
+            alert(res.message);
+          }
+        });
+    }
+  }
+
+  onEdit(client: Client) {
+    this.clientObj = client;
+  }
 }
