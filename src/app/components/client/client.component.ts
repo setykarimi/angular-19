edddc7b69a32537c3feb1067e1cf851c2756dbdb
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-08 09:00:05 +0330

    🌿feat: Create client Obj in client page

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Client } from '../../model/class/Client';
 
 @Component({
   selector: 'app-client',
   imports: [],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
 export class ClientComponent {
-
+  clientObj: Client = new Client();
+  clientList: Client[] = [];
 }
