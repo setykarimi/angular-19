c9d255bac97cb4771122a9f1b130766f4d1a3335
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-08 13:50:05 +0330

    feat: Add form for client update or client

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css',
 })
 export class ClientComponent {
   clientObj: Client = new Client();
   clientList: Client[] = [];
+
+
+  onSaveClient(){
+
+  }
 }
