4653f08d573e9a34bf91ab1811a9399a5725049e
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-09 10:00:45 +0330

    🌿feat: Create new components for client-project

---
+++ src/app/components/client-project/client-project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-project',
+  imports: [],
+  templateUrl: './client-project.component.html',
+  styleUrl: './client-project.component.css'
+})
+export class ClientProjectComponent {
+
+}
