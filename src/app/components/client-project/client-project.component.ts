f0095bf689c7fe3641e259d482ee73b7540877b4
ISQI\se.karimi <setykarimi80@gmail.com>
2025-06-09 10:30:00 +0330

    🌿feat: Edit form for client project component

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-project',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './client-project.component.html',
-  styleUrl: './client-project.component.css'
+  styleUrl: './client-project.component.css',
 })
 export class ClientProjectComponent {
-
+  projectForm: FormGroup = new FormGroup({
+    clientProjectId: new FormControl(0),
+    prjectName: new FormControl(''),
+    startDate: new FormControl(''),
+    expectedEndData: new FormControl(''),
+    leadByEmpId: new FormControl(''),
+    completedDate: new FormControl(''),
+    contactPerson: new FormControl(''),
+    contactPersonContactNo: new FormControl(''),
+    totalEmpWorking: new FormControl(''),
+    projectCost: new FormControl(''),
+    projectDetails: new FormControl(''),
+    contactPersonEmailId: new FormControl(''),
+    clientId: new FormControl(''),
+  });
 }
